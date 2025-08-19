d727d3a96b0a94cb4538831daed19272de7167bc
Abbey Yacoe <abbey@xyflow.com>
2025-08-19 15:02:32 +0200

    fix(vite.config): resolve paths correctly for Windows machines

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
+import { fileURLToPath } from 'url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': path.resolve(
-        path.dirname(new URL(import.meta.url).pathname),
-        './src'
-      ),
+      '@': path.resolve(fileURLToPath(new URL('.', import.meta.url)), 'src'),
     },
   },
   base: 'https://flow-machine-xyflow.vercel.app/',
