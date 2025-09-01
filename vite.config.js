547802f4ece975e0942b6c775982beb1ff32168a
Abbey Yacoe <abbey@xyflow.com>
2025-09-01 13:23:07 +0200

    chore: fix base url

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,18 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
 import { fileURLToPath } from 'url';
+import process from 'node:process';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig(() => ({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(fileURLToPath(new URL('.', import.meta.url)), 'src'),
     },
   },
-  base: mode === 'production' ? 'https://flow-machine-xyflow.vercel.app/' : '/',
+  base:
+    process.env.VERCEL_ENV === 'production'
+      ? 'https://flow-machine-xyflow.vercel.app/'
+      : '/',
 }));
